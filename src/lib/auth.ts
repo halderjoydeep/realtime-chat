cf6c577f4d0457e91df2c91d559df85ff79bceee
Joydeep Halder <42032921+halderjoydeep@users.noreply.github.com>
2025-01-24 23:40:26 +0530

    feat: setup next-auth and register route

---
+++ src/lib/auth.ts
@@ -0,0 +1,55 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import bcrypt from 'bcrypt';
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import GitHub from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+import { prisma } from './prisma';
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GitHub,
+    Google,
+    Credentials({
+      name: 'credentials',
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+
+        if (!email || !password) {
+          throw new Error('Invalid Credentials');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error('Invalid Credentials');
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          password,
+          user.hashedPassword,
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error('Invalid Credentials');
+        }
+
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === 'development',
+});
